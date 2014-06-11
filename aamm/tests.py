e60677a5069ba40af7b05089d14167820e4ebdf5
george <rpubaddr0@gmail.com>
2014-06-11 13:05:19 -0600

    Removed old test.

--- aamm/tests.py
+++ aamm/tests.py
@@ -9,9 +9,3 @@ class ViewTests(unittest.TestCase):
 
     def tearDown(self):
         testing.tearDown()
-
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'aamm')
